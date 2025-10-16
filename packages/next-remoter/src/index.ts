e1c9b743d4345c8e7726db0aa3604a6cee63f81b
shenjunjian <40288193@qq.com>
2025-10-16 17:33:24 +0800

    fix(useNextAgent): 添加 useNextAgent的导出

--- packages/next-remoter/src/index.ts
+++ packages/next-remoter/src/index.ts
@@ -1,9 +1,10 @@
 import TinyRemoter from './components/tiny-robot-chat.vue'
 import '@opentiny/tiny-robot/dist/style.css'
+import { useNextAgent } from './composable/useNextAgent'
 export * from './types/type'
 
 TinyRemoter.install = function (Vue: any) {
   Vue.component('tiny-remoter', TinyRemoter)
 }
 
-export { TinyRemoter, MateChatRemoter }
+export { TinyRemoter, useNextAgent }
