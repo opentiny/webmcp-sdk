d4ddae37ee2da72ba4ae2ab446517ab57503c32c
shenjunjian <40288193@qq.com>
2025-10-27 11:19:24 +0800

    fix(wxt): fix

--- packages/next-wxt/entrypoints/content.ts
+++ packages/next-wxt/entrypoints/content.ts
@@ -1,38 +1,9 @@
 import { onMessage, sendMessage } from 'webext-bridge/content-script'
 
 export default defineContentScript({
-  // world: "ISOLATED", // Main 不支持V2, 不支持ff
   // matches: ["<all_urls>"],
   // matches: ["*://*/*"],
   matches: ['*://*.baidu.com/*'],
   runAt: 'document_end',
-  async main() {
-    console.log('Hello content.')
-
-    // async function testStorage() {
-    //   storage.setItem("local:ext-content-data", "1");
-    //   storage.setMeta("local:ext-content-meta", { date: Date.now() });
-
-    //   console.log("[content]", await storage.getItems(["local:ext"]));
-    // }
-
-    // testStorage();
-
-    // console.log("[content]", document.querySelectorAll("div"));
-
-    // 发送消息
-    const bgResponse = await browser.runtime.sendMessage({
-      type: 'hello',
-      name: 'Shenjunjian'
-    })
-    onMessage('foo', ({ data }) => {
-      // type of `data` will be `{ title: string }`
-      console.log(data.title)
-    })
-
-    const returnData = await sendMessage('bar', {
-      /* ... */
-    })
-    // console.log("[content]:  bg answer", bgResponse);
-  }
+  async main() {}
 })
--- packages/next-wxt/utils/common.ts
+++ packages/next-wxt/utils/common.ts
@@ -1,4 +1,4 @@
-// 写一些公共方法，自动导入到全局
+// 写一些公共方法，自动导入到全局，比如查询tabs
 // export const common = () => {
 //   console.log('in common')
 // }
