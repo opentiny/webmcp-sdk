b09a51fb72124b0713af2967939411153844db57
ajaxzheng <894103554@qq.com>
2025-11-04 16:31:27 +0800

    feat: enhance next-wxt scripts and configuration for improved development and build processes

--- packages/next-wxt/entrypoints/sidepanel/App.vue
+++ packages/next-wxt/entrypoints/sidepanel/App.vue
@@ -1,11 +1,26 @@
 <script lang="ts" setup>
 import { TinyRemoter } from '@opentiny/next-remoter'
 import { useBrowserExtensions } from './useBrowserExtensions'
+
+const llmConfig = {
+  apiKey: import.meta.env.VITE_LLM_API_KEY,
+  baseURL: import.meta.env.VITE_LLM_BASE_URL,
+  providerType: import.meta.env.VITE_LLM_PROVIDER_TYPE,
+  model: import.meta.env.VITE_LLM_MODEL
+}
 </script>
 
 <template>
   <div>
-    <TinyRemoter mode="chat-dialog" :browserExtensions="useBrowserExtensions" show fullscreen title=""> </TinyRemoter>
+    <TinyRemoter
+      mode="chat-dialog"
+      :browserExtensions="useBrowserExtensions"
+      show
+      fullscreen
+      title=""
+      :llmConfig="llmConfig"
+    >
+    </TinyRemoter>
   </div>
 </template>
 
