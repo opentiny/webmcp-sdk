81f522e607c01a33b2f4797f1d520a7f8392d3d2
申君健 <40288193@qq.com>
2025-12-11 09:37:55 +0800

    fix(wxt): 添加 LLM 出错时,能够直接结束对话 (#249)

--- packages/next-remoter/src/composable/CustomAgentModelProvider.ts
+++ packages/next-remoter/src/composable/CustomAgentModelProvider.ts
@@ -349,6 +349,24 @@ export class CustomAgentModelProvider extends BaseModelProvider {
     let textId = 1
     let thinkId = 1
     for await (const part of result.fullStream) {
+      // 处理错误， 暂时模拟 AI 回复消息。 TODO: robot 设计出错效果
+      if (part.type === 'error') {
+        const message = part.error?.data?.error?.message || part.error?.message || '访问大模型出错'
+        handler.onData({
+          type: 'markdown',
+          content: '',
+          delta: '',
+          textId
+        } as any)
+
+        handler.onData({
+          type: 'markdown',
+          delta: message,
+          textId
+        } as any)
+        handler.onError(message)
+      }
+
       // 开始节点处理
       if (part.type.includes('start-') || part.type.includes('-start')) {
         handler.onData({ type: 'start' } as any)
