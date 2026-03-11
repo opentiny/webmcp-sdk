7694023b512f439c31d0de37ad51282cb3768144
shenjunjian <40288193@qq.com>
2026-03-11 16:15:14 +0800

    fix: 整合 skills 与路由管理

--- packages/doc-ai-react/src/main.tsx
+++ packages/doc-ai-react/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { setNavigator } from '@opentiny/next-sdk'
 import './index.css'
 import App from './App.tsx'
 
