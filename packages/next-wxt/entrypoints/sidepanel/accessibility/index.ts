f3a4ed326122624ddf1574960218e27e96caeee2
申君健 <40288193@qq.com>
2026-04-06 17:55:15 +0800

    fix: 修改无障碍边框引起的其它工具使用异常 (#417)

--- packages/next-wxt/entrypoints/sidepanel/accessibility/index.ts
+++ packages/next-wxt/entrypoints/sidepanel/accessibility/index.ts
@@ -22,7 +22,6 @@ export async function executeSnapshotAction(params: SnapshotActionParams): Promi
   try {
     // 根据操作类型分发到不同的处理函数
     if (action === 'snapshot') {
-      await handleSnapshotAction(manager)
       await manager.highlightPage(true) // 每一次查询无障碍，都高亮一次页面。
 
       return await handleSnapshotAction(manager)
