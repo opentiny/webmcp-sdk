de8c00d5c7ee4dfb0fa3871bd291767998371396
shenjunjian <40288193@qq.com>
2026-07-14 19:35:34 +0800

    fix: 修改语法错误

--- packages/next-sdk/page-tools/page-agent-tool.ts
+++ packages/next-sdk/page-tools/page-agent-tool.ts
@@ -142,6 +142,7 @@ export function registerPageAgentTool(options: PageAgentToolOptions = {}) {
     // @ts-ignore
     inputSchema: zodToJsonSchema(inputSchema) as any,
     async execute(args: PageAgentToolInput) {
+      let ret: any
       try {
         switch (args.action) {
           case 'browserState':
@@ -149,14 +150,14 @@ export function registerPageAgentTool(options: PageAgentToolOptions = {}) {
           case 'click':
             await pageController.showMask()
             pageController.mask.borderElement(currentRefMap.get(args.index))
-            const ret = await handleClick(args, actionContext)
+            ret = await handleClick(args, actionContext)
             pageController.mask.removeBorderElement()
             await pageController.hideMask()
             return ret
           case 'fill':
             await pageController.showMask()
             pageController.mask.borderElement(currentRefMap.get(args.index))
-            const ret = await handleFill(args, actionContext)
+            ret = await handleFill(args, actionContext)
             pageController.mask.removeBorderElement()
 
             await pageController.hideMask()
@@ -164,13 +165,13 @@ export function registerPageAgentTool(options: PageAgentToolOptions = {}) {
           case 'select':
             await pageController.showMask()
             pageController.mask.borderElement(currentRefMap.get(args.index))
-            const ret = await handleSelect(args, actionContext)
+            ret = await handleSelect(args, actionContext)
             pageController.mask.removeBorderElement()
             await pageController.hideMask()
             return ret
           case 'scroll':
             await pageController.showMask()
-            const ret = await handleScroll(args, actionContext)
+            ret = await handleScroll(args, actionContext)
             await pageController.hideMask()
             return ret
           case 'executeJavascript':
