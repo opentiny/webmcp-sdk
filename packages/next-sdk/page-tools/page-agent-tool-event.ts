8d0211e463765d25ff29510a89a60c64c8067d7f
shenjunjian <40288193@qq.com>
2026-07-28 11:18:20 +0800

    fix: 遮罩层允许用户点击穿透，增加用户操作反馈的事件

--- packages/next-sdk/page-tools/page-agent-tool-event.ts
+++ packages/next-sdk/page-tools/page-agent-tool-event.ts
@@ -1,9 +1,11 @@
+import { ActionContext } from './context'
 import { inputSchema, type PageAgentToolInput, type PageAgentToolRawInput } from './schema'
 import { PageController } from '@page-agent/page-controller'
 
 export const PAGE_AGENT_TOOL_CALL_EVENT = 'page-agent-tool-call'
 export const PAGE_AGENT_TOOL_RESULT_EVENT = 'page-agent-tool-result'
 export const PAGE_AGENT_CHAT_END_EVENT = 'page-agent-chat-end'
+export const PAGE_AGENT_USER_DO_ACTION_EVENT = 'page-agent-user-do-action' // 用户执行click 等操作事件
 
 export type PageAgentToolCallEventDetail = {
   data?: PageAgentToolRawInput
@@ -44,7 +46,8 @@ function dispatchPageAgentToolResult(detail: PageAgentToolResultEventDetail) {
 
 export function setupPageAgentToolEventBridge(
   executePageAgentTool: ExecutePageAgentTool,
-  pageController: PageController
+  pageController: PageController,
+  actionContext: ActionContext
 ) {
   window.__nextSdkPageAgentToolEventCleanup?.()
 
@@ -102,12 +105,28 @@ export function setupPageAgentToolEventBridge(
     } catch (error) {}
   }
 
+  const handleUserDoAction = async (ev: MouseEvent) => {
+    // @ts-ignore
+    if (ev.isTrusted && pageController.mask.shown) {
+      // 通过ev.target找到 refMap 对应的元素
+      const target = ev.target as HTMLElement
+      const refMap = actionContext.getRefMap()
+      const targetParent = Array.from(refMap.values()).find((el) => el.contains(target)) as HTMLElement | undefined
+
+      if (targetParent) {
+        window.dispatchEvent(new CustomEvent(PAGE_AGENT_USER_DO_ACTION_EVENT, { detail }))
+      }
+    }
+  }
+
   window.addEventListener(PAGE_AGENT_TOOL_CALL_EVENT, handleToolCall)
   window.addEventListener(PAGE_AGENT_CHAT_END_EVENT, handleChatEnd)
+  window.addEventListener('click', handleUserDoAction, true)
 
   window.__nextSdkPageAgentToolEventCleanup = () => {
     window.removeEventListener(PAGE_AGENT_TOOL_CALL_EVENT, handleToolCall)
     window.removeEventListener(PAGE_AGENT_CHAT_END_EVENT, handleChatEnd)
+    window.removeEventListener('click', handleUserDoAction, true)
     window.__nextSdkPageAgentToolEventCleanup = undefined
   }
 }
