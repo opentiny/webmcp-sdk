675e84a46dd68480e681aefe0e262f4c42b02dc4
zzcr <894103554@qq.com>
2026-08-17 14:28:26 +0800

    chore: delete useless test

--- packages/next-sdk/vitest-order-test.ts
+++
@@ -1,9 +0,0 @@
-import { test, vi, expect } from 'vitest'
-
-const mock1 = vi.fn()
-const mock2 = vi.fn()
-
-mock1()
-mock2()
-
-console.log(mock1.mock.invocationCallOrder[0], mock2.mock.invocationCallOrder[0])
